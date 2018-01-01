f3c909a3d24ad5fd341fdd8b46eb02fdf2197636
jxmot <webdev@stupiddog.net>
2017-12-31 19:42:35 -0600

    receiving notifications when new sensor data is added

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -2,3 +2,17 @@
 /*
     Firebase - functions for interfacing with firebase
 */
+/*
+*/
+gSensorData.orderByChild('tstamp').limitToLast(1).on('child_added', newSensorData);
+
+
+
+function newSensorData(snapShot) {
+
+    console.log("data added!!!");
+
+};
+
+
+
