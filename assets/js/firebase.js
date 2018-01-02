ac968f6e1119713dda852e0e0cbe8247367805d9
jxmot <webdev@stupiddog.net>
2018-01-01 23:44:14 -0600

    added data and status event triggers

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -6,12 +6,28 @@
 */
 gSensorData.orderByChild('tstamp').limitToLast(1).on('child_added', newSensorData);
 
-
-
 function newSensorData(snapShot) {
+    console.log('data added!!!');
+    console.log(JSON.stringify(snapShot.val()));
+    console.log();
 
-    console.log("data added!!!");
+    var data = JSON.parse(JSON.stringify(snapShot.val()));
 
+    $(document).trigger(data.hostname, data);
+};
+
+/*
+*/
+gSensorStatus.orderByChild('tstamp').limitToLast(1).on('child_added', newSensorStatus);
+
+function newSensorStatus(snapShot) {
+    console.log('status added!!!');
+    console.log(JSON.stringify(snapShot.val()));
+    console.log();
+
+    var status = JSON.parse(JSON.stringify(snapShot.val()));
+
+    $(document).trigger(status.hostname+'_status', status);
 };
 
 
