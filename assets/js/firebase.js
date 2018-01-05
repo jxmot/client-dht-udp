52f720b9c8d0a99138ca3419a809751d83e848b2
jxmot <webdev@stupiddog.net>
2018-01-04 22:02:59 -0600

    comments, removed some console.log() calls

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -3,6 +3,7 @@
     Firebase - functions for interfacing with firebase
 */
 /*
+    Wait for new data to be written to the sensor data log
 */
 gSensorData.orderByChild('tstamp').limitToLast(1).on('child_added', newSensorData);
 
@@ -17,17 +18,39 @@ function newSensorData(snapShot) {
 };
 
 /*
+    Wait for new statuses to be written to the sensor status log
 */
 gSensorStatus.orderByChild('tstamp').limitToLast(1).on('child_added', newSensorStatus);
 
 function newSensorStatus(snapShot) {
-    console.log('status added!!!');
-    console.log(JSON.stringify(snapShot.val()));
-    console.log();
-
     var status = JSON.parse(JSON.stringify(snapShot.val()));
-
     $(document).trigger(status.hostname+'_status', status);
+};
+
+
+/*
+    In situations where a gauge (google gauges) isn't seen until its 
+    first update this will obtain the last record for a specified
+    gauge. And then the event is triggered and the gauge will update
+    and display.
+*/
+// Gauges are configured in pairs, one for temperature and one for
+// humidity. A single event trigger will update both, but since this
+// function is called during gauge initialization it will be called
+// twice with the same `hostname`. This will aid in preventing a 
+// gauge from being updated twice.
+var lasthostname = '';
+
+function firebase_initGauge(hostname) {
+
+    if(lasthostname === hostname) return;
+    lasthostname = hostname;
+
+    gSensorData.orderByChild('hostname').equalTo(hostname).limitToLast(1).once('value', function(snapShot) {
+        snapShot.forEach(function(data) {
+            $(document).trigger(data.val().hostname, data.val());
+        });
+    });
 };
 
 
