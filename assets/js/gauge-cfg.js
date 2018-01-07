71ee1919ddd22c86b7c4111c6079a43a69a7a5f4
jxmot <webdev@stupiddog.net>
2018-01-07 02:47:18 -0600

    removed thingspeak gauge configs

--- assets/js/gauge-cfg.js
+++ assets/js/gauge-cfg.js
@@ -26,52 +26,8 @@ var _enable = function() {
 var gauge_cfg = [
     // 
     {
-        target: 'gauge_div',
-        name:'Object',
-        type: 'T',                      // or 'H'
-        unit: 'F',                      // or 'C' or '%' <-- get from incoming data, req's mods
-        //data_source: 'thingspeak',      // or 'firebase' or ????
-        data_source: '',      // 'thingspeak' or 'firebase' or ????
-        data_channel: 'field2',         // the thingspeak field, or use a unique ID, this could be "name" or "loc"
-        round: true,                    // true = round the value before displaying it
-        opt: {
-            min: 25, max: 120,          // the range of the gauge
-            width: 180, height: 180,    // size
-            yellowColor: 'blue',        // change the color of the defined `yellow` sector
-            yellowFrom:25, yellowTo: 55,
-            greenFrom: 55, greenTo: 80,
-            redFrom: 80, redTo: 120,
-            minorTicks: 5
-        },
-        chart: {},
-        data: {}
-    },
-    // 
-    {
-        target: 'gauge_div2',
-        name:'Ambient',
-        type: 'T',                      // or 'H'
-        unit: 'F',                      // or 'C' or '%' <-- get from incoming data, req's mods
-        //data_source: 'thingspeak',      // or 'firebase' or ???? (if set to '' then gauge is ignored)
-        data_source: '',      // 'thingspeak' or 'firebase' or ???? (if set to '' then gauge is ignored)
-        data_channel: 'field1',         // the thingspeak field, or use a unique ID, this could be "(host)name" or "loc"
-        round: false,                   // true = round the value before displaying it
-        opt: {
-            min: 25, max: 120, 
-            width: 180, height: 180,
-            yellowColor: 'blue',
-            yellowFrom:25, yellowTo: 55,
-            greenFrom: 55, greenTo: 80,
-            redFrom: 80, redTo: 120,
-            minorTicks: 5
-        },
-        chart: {},
-        data: {}
-    },
-    // 
-    {
         target: 'gauge_div3',
-        name:'Office',
+        name:'Den',
         type: 'T',
         unit: 'F',
         data_source: 'firebase',
@@ -93,7 +49,7 @@ var gauge_cfg = [
     // 
     {
         target: 'gauge_div4',
-        name:'Office',
+        name:'Den',
         type: 'H',
         unit: '%',
         data_source: 'firebase',
@@ -200,7 +156,7 @@ var gauge_cfg = [
     // 
     {
         target: 'gauge_div9',
-        name:'Den',
+        name:'Office',
         type: 'T',
         unit: 'F',
         data_source: 'firebase',
@@ -222,7 +178,7 @@ var gauge_cfg = [
     // 
     {
         target: 'gauge_div10',
-        name:'Den',
+        name:'Office',
         type: 'H',
         unit: '%',
         data_source: 'firebase',
