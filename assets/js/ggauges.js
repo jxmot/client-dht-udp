a109ed320933c315d4106099349c55304cd06711
jxmot <webdev@stupiddog.net>
2018-01-08 05:48:28 -0600

    comments, minor changes

--- assets/js/ggauges.js
+++ assets/js/ggauges.js
@@ -1,6 +1,6 @@
 // load the google gauge visualization - google API load 
 google.load('visualization', '1', {packages:['gauge']});
-google.setOnLoadCallback(window.initGauges);
+google.setOnLoadCallback(initGauges);
 
 // display the data from ThingSpeak or a similar data souce
 // directly to an indexed gauge
@@ -15,29 +15,32 @@ function displayData(ix, point) {
 
 // initialize the guages...
 function initGauges() {
+    // initialize all gauges...
     for(var ix = 0; ix < gauge_cfg.length; ix++)
     {
         gauge_cfg[ix].data = new google.visualization.DataTable();
         gauge_cfg[ix].data.addColumn('string', 'Label');
         gauge_cfg[ix].data.addColumn('number', 'Value');
         gauge_cfg[ix].data.addRows(1);
-
+        // attach the gauge to its DOM target
         gauge_cfg[ix].chart = new google.visualization.Gauge(document.getElementById(gauge_cfg[ix].target));
-
+        // set the gauge label, this will be the configured name plus its type
         gauge_cfg[ix].data.setValue(0, 0, gauge_cfg[ix].name + ' ' + gauge_cfg[ix].type);
-
-        // NOTE: This is done here as a placeholder for future expansion. Not 
-        // all of the may use Thingspeak as their data source. In the future
-        // (and likely a different project) may use Firebase as the data source
-        //
-        // This will start a repeating "read" of Thingspeak data, with an
-        // interval that's configured in _thingspk-cfg.js
+        // choose the appropriate initialization based on the data source 
+        // for the gauge
         if('thingspeak' === gauge_cfg[ix].data_source) {
-            window.thingspk_loadData(ix);
-            setInterval(window.thingspk_loadData, thingspk_cfg.interval, ix);
+            // This will start a repeating "read" of Thingspeak data, with an
+            // interval that's configured in _thingspk-cfg.js
+            thingspk_loadData(ix);
+            setInterval(thingspk_loadData, thingspk_cfg.interval, ix);
         } else if('firebase' === gauge_cfg[ix].data_source) {
+            // This gauge uses Firebase. Enable the gauge to receive updates from
+            // the database as records are written to it.
             gauge_cfg[ix].enable();
-            window.firebase_initGauge(gauge_cfg[ix].data_channel);
+            // The enable() function will read the last written record for only
+            // the most recent sensor. This will insure that each gauge is updated
+            // when it's created.
+            firebase_initGauge(gauge_cfg[ix].data_channel);
         }
     }
 };
